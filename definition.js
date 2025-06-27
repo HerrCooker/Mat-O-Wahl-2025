18b8fbbf790be74c3e99db1626256595bc32f5ab
Jan <jan.koch@stud.ui-due.de>
2025-06-27 23:58:22 +0200

    Final version 2025

--- definition.js
+++ definition.js
@@ -231,8 +231,7 @@ const imprintDisputeResultion = `<p>Die Europäische Kommission stellt eine Plat
 //	(optional) Redaktion: Person(en), die die Fragen ausgearbeitet hat
 //	(optional) Editor: Person(s), who worked on the questions
 
-const imprintEditors = `<p>Max Mustermann, Martina Mustermann, Harry Hirsch</p>
-		     <p> <a href='mailto:max@mustermann-politikfreund.de'>max@mustermann-politikfreund.de</a></p>`;
+const imprintEditors = `<p>Moritz Karrer, Jan Koch</p>`;
 
 
 //	1.7.B.6
